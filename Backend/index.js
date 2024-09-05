72e5180a7e1ea8bd8cfa083d78042e3261451e97
gargriya01 <72300799+gargriya01@users.noreply.github.com>
2024-09-05 13:16:07 +0530

    BACKEND ALL DONE !!

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,73 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+ const PORT =  process.env.PORT || 4000;
+ const URI = process.env.MongoDBURI;
 
-app.get("/", (req, res) => {
-  res.send("BookStore App");
-});
+//  CONNECT TO MONGODB
+try {
+  mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("Connected to mongoDB");
+} catch (error) {
+  console.log("Error: ", error);
+}
+
+// defining routes
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const app = express();
+// const PORT = process.env.PORT || 4000;
+// const URI = process.env.MongoDBURI;
+
+// // CONNECT TO MONGODB
+// mongoose
+//   .connect(URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//   });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,66 @@
-import React from 'react';
+// import React from 'react';
+// import Home from "./home/Home";
+// import { Route, Routes } from "react-router-dom";
+// import Courses from './courses/Courses';
+// import Contact from './components/Contact';
+// import Signup from "./components/Signup";
+// import Login from "./components/Login";
+// import toast, { Toaster } from 'react-hot-toast';
+// import { useAuth } from "./context/AuthProvider";
+
+// function App() {
+//   const [authUser, setAuthUser] = useAuth();
+//   console.log(authUser);
+//   return (
+//      <>
+//       <div className=" dark:bg-slate-900 dark:text-white">
+//   <Routes>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/course" element={<Courses />} />
+//     <Route path="/login" element={<Login/>} />
+//     <Route path="/contact" element={<Contact/>} />
+//     <Route path="/signup" element={<Signup />} />
+//     </Routes>
+//     <Toaster/>
+//     </div>
+// </>
+// );
+
+// }
+
+// export default App;
+import React from "react";
 import Home from "./home/Home";
-import { Route, Routes } from "react-router-dom";
-import Courses from './courses/Courses';
+import { Navigate, Route, Routes } from "react-router-dom";
+import Courses from "./courses/Courses";
 import Contact from './components/Contact';
 import Signup from "./components/Signup";
-import Login from "./components/Login";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider";
 
 function App() {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
-     <>
-      <div className=" dark:bg-slate-900 dark:text-white">
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/course" element={<Courses />} />
-    <Route path="/login" element={<Login/>} />
-    <Route path="/contact" element={<Contact/>} />
-    <Route path="/signup" element={<Signup />} />
-
-    </Routes>
-    </div>
-</>
-);
-
+    <>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+           <Route path="/contact" element={<Contact/>} />
+          <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          <Route path="/signup" element={<Signup />} />
+          
+          
+        </Routes>
+        
+        <Toaster />
+      </div>
+    </>
+  );
 }
 
 export default App;
+
